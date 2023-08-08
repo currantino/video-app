a40c6d03b2ac08ab95839bf8dd7bcdaa4a02cbb8
Matvey Smorodin <mtv.smorodin@gmail.com>
2023-08-08 15:53:04 +0400

    Add POST /upload endpoint

--- src/main/kotlin/com/currantino/Application.kt
+++ src/main/kotlin/com/currantino/Application.kt
@@ -1,9 +1,13 @@
 package com.currantino
 
-import com.currantino.plugins.*
+import com.currantino.koin.appModule
+import com.currantino.plugins.configureRouting
+import com.currantino.plugins.configureSerialization
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import org.koin.ktor.plugin.Koin
+import org.koin.logger.slf4jLogger
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
@@ -13,4 +17,9 @@ fun main() {
 fun Application.module() {
     configureSerialization()
     configureRouting()
+    videoRouting()
+    install(Koin) {
+        slf4jLogger()
+        modules(appModule)
+    }
 }
---
+++ src/main/kotlin/com/currantino/controller.VideoController.kt
@@ -0,0 +1,22 @@
+package com.currantino
+
+import com.currantino.service.VideoService
+import io.ktor.server.application.*
+import io.ktor.server.plugins.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import org.koin.ktor.ext.inject
+
+fun Application.videoRouting() {
+    val videoService by inject<VideoService>()
+    routing {
+        post("/upload") {
+            val data = call.receiveMultipart()
+            val contentLength =
+                call.request.contentLength() ?: throw BadRequestException("Provide content-length header.")
+            val response = videoService.uploadVideo(data, contentLength)
+            call.respond(response)
+        }
+    }
+}
